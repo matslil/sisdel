566201a452aff3ddf4e65724a4fd6a9307087c9e
Mats Liljegren <liljegren.mats@gmail.com>
2017-01-05 23:10:40 +0100

    check_mmap_file: Added file-not-found and skip unit tests

--- unit-test/check_mmap_file.cc
+++ unit-test/check_mmap_file.cc
@@ -64,6 +64,53 @@ namespace {
 		EXPECT_THROW(mmap_file_t file(m_env, m_env.sbucket().find_add("non-existing-file")), std::system_error);
 	}
 
+	std::string first_line(const char *str)
+	{
+		std::stringstream ss(str);
+		std::string retstr;
+
+		getline(ss, retstr);
+		return retstr;
+	}
+
+#define MY_EXPECT_EQ(a,b) do { SCOPED_TRACE("MY_EXPECT_EQ"); expect_eq(a,b); } while(0)
+	
+	template <typename T>
+	inline void expect_eq(const T t1, const T t2)
+	{
+		EXPECT_EQ(t1, t2);
+	}
+	
+	TEST(test_mmap_file, skip) {
+		environment_t env;
+		mmap_file_t file(env, env.sbucket().find_add("check_mmap_file.data"));
+
+		// Check that we are at the beginning of the file
+		const std::string line1("# Some test data used for the check_mmap_file unit test");
+		MY_EXPECT_EQ(first_line(file.get_position().m_buff), line1);
+		
+		// Test skip_until(chr)
+		file.skip_until('\n');
+		
+		// Test skip(chr)
+		// Skip all new-lines, expect two of them
+		MY_EXPECT_EQ(static_cast<size_t>(2), file.skip('\n'));
+
+		// Check that we are at line 3
+		const std::string line3("adf lkjdsf asdfkdsafjh sakdfhkjashfd");
+		MY_EXPECT_EQ(first_line(file.get_position().m_buff), line3);
+		MY_EXPECT_EQ(static_cast<size_t>(3), file.get_position().m_line);
+		MY_EXPECT_EQ(static_cast<size_t>(1), file.get_position().m_col);
+
+		// Test skip(str)
+		// Check that skipping all characters on current line takes us
+		// to the new-line character
+		MY_EXPECT_EQ(line3.length(), file.skip(line3.c_str()));
+		MY_EXPECT_EQ(static_cast<size_t>(3), file.get_position().m_line);
+		MY_EXPECT_EQ(line3.length()+1, file.get_position().m_col);
+		MY_EXPECT_EQ('\n', *file.get_position().m_buff);
+		
+	}
 }
 
 int main(int argc, char *argv[]) {
