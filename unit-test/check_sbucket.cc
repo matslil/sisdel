6fc77218bd1e0e52751f780bd1ea19d35cc715db
Mats Liljegren <liljegren.mats@gmail.com>
2017-01-07 20:41:35 +0100

    check_sbucket.cc: Added possibility for manual test strings

--- unit-test/check_sbucket.cc
+++ unit-test/check_sbucket.cc
@@ -37,30 +37,43 @@ namespace {
 			// Variant 1 is for the find_add test case
 			for (size_t variant = 0; variant < 2; variant++) {
 				for (size_t i = 0; i < m_nr_strings; i++) {
-					// Determine length of the string
-					const size_t size = std::min(
-						static_cast<size_t>(n(r)), m_max_size);
+					std::string test_str;
+					if (i == 0)
+						test_str = "token1";
+					else if (i == 1)
+						test_str = "token2";
+					else {
+						
+						// Determine length of the string
+						const size_t size = std::min(
+							static_cast<size_t>(n(r)), m_max_size);
 
-					// Hash is only used for variant 0
-					hash_t hash = 0;
-					for (size_t c = 0; c < size; c++) {
-						// Random character
-						const char ch = static_cast<char>(u(r));
-						// Store character into string
-						m_strings[variant][i].push_back(ch);
-						if (variant == 0)
-							hash = hash_next(ch, hash);
+						// Hash is only used for variant 0
+						for (size_t c = 0; c < size; c++) {
+							// Random character
+							const char ch = static_cast<char>(u(r));
+							test_str.push_back(ch);
+						}
 					}
+					// Store test string
+					m_strings[variant][i] = test_str;
+
+					// Calculate hash
 					if (variant == 0) {
-						hash = hash_finish(hash);
-						m_hash[i] = hash;
+						hash_t hash = 0;
+						for (size_t c = 0; c < test_str.length(); c++) {
+							// Random character
+							const char ch = static_cast<char>(u(r));
+							if (variant == 0)
+								hash = hash_next(ch, hash);
+						}
 					}
 				}
 			}
 		}
 
 		// Number of strings. Becomes * 2 since we have two variants
-		static const size_t m_nr_strings = 32 * 1024;
+		static const size_t m_nr_strings = 1024;
 
 		// The actual strings with random characters
 		std::array<std::string, m_nr_strings> m_strings[2];
