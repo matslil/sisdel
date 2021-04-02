7613b62e3ede21dc547f176f67216d0843460cd2
Mats Liljegren <liljegren.mats@gmail.com>
2021-04-02 10:49:37 +0200

    Switch from gtest to catch2, add Docker support

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,23 @@
 FROM bitnami/minideb:buster
 
+ADD https://github.com/Kitware/CMake/releases/download/v3.20.0/cmake-3.20.0-linux-x86_64.sh /
+
+RUN chmod +x /cmake-3.20.0-linux-x86_64.sh && /cmake-3.20.0-linux-x86_64.sh --skip-license --prefix=/usr && rm /cmake-3.20.0-linux-x86_64.sh
+
 # Make image podman friendly, move nogroup to lower part of gid
 RUN OLD_GID=$(getent group sys | cut -d: -f3) && \
     groupmod -g 200 nogroup && \
     find / -mount -group ${OLD_GID} -exec chgrp -h nogroup {} \;
 
 # Install non-python build tools
-RUN install_packages clang clang-format clang-tidy lldb llvm python3-minimal python3-venv python3-pip cmake ninja-build libboost-all-dev doxygen graphviz pkg-config libgmp-dev liblttng-ust-dev
+RUN install_packages clang clang-format clang-tidy lldb llvm python3-minimal python3-venv python3-pip ninja-build libboost-all-dev doxygen graphviz pkg-config libgmp-dev liblttng-ust-dev lcov git libmpfr-dev
+
+RUN git clone https://github.com/catchorg/Catch2.git && \
+    cd Catch2 && \
+    cmake -G Ninja -Bbuild -H. -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=/usr && \
+    cmake --build build/ --target install && \
+    cd .. && \
+    rm -rf Catch2
 
 # Setup virtual environment for Python
 RUN python3 -m venv /venv
--- src/unit-test/check_mmap_file.cc
+++ src/unit-test/check_mmap_file.cc
@@ -19,57 +19,43 @@
   under the License.
  */
 
-#include <gtest/gtest.h>
+#include <catch2/catch_all.hpp>
 #include "mmap_file.hh"
 
-namespace {
-	TEST(test_mmap_file, file_not_found) {
-		environment_t m_env;
-		EXPECT_THROW(mmap_file_t file(m_env, "non-existing-file"), std::system_error);
-	}
-
-#define MY_EXPECT_EQ(a,b) do { SCOPED_TRACE("MY_EXPECT_EQ"); expect_eq(a,b); } while(0)
-	
-	template <typename T>
-	inline void expect_eq(const T t1, const T t2)
-	{
-		EXPECT_EQ(t1, t2);
-	}
-	
-	TEST(test_mmap_file, skip) {
-		environment_t env;
-		mmap_file_t file(env, "check_mmap_file.data");
-
-		// Check that we are at the beginning of the file
-		const std::string line1("# Some test data used for the check_mmap_file unit test");
-		MY_EXPECT_EQ(file.get_position().str(), line1);
-		
-		// Test skip_until(chr)
-		file.skip_until('\n');
-		
-		// Test skip(chr)
-		// Skip all new-lines, expect two of them
-		MY_EXPECT_EQ(static_cast<size_t>(2), file.skip('\n'));
-
-		// Check that we are at line 3
-		const std::string line3("adf lkjdsf asdfkdsafjh sakdfhkjashfd");
-		MY_EXPECT_EQ(file.get_position().str(), line3);
-		MY_EXPECT_EQ(static_cast<size_t>(3),
-			     file.get_position().line());
-		MY_EXPECT_EQ(static_cast<size_t>(1),
-			     file.get_position().column());
-
-		// Test skip(str)
-		// Check that skipping all characters on current line takes us
-		// to the new-line character
-		MY_EXPECT_EQ(line3.length(), file.skip(line3.c_str()));
-		MY_EXPECT_EQ(static_cast<size_t>(3),
-			     file.get_position().line());
-		MY_EXPECT_EQ(line3.length()+1, file.get_position().column());
-	}
+TEST_CASE("test_mmap_file:file_not_found") {
+	environment_t m_env;
+	REQUIRE_THROWS_AS(mmap_file_t(m_env, "non-existing-file"), std::system_error);
 }
 
-int main(int argc, char *argv[]) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+TEST_CASE("test_mmap_file:skip") {
+	environment_t env;
+	mmap_file_t file(env, "check_mmap_file.data");
+
+	// Check that we are at the beginning of the file
+	const std::string line1("# Some test data used for the check_mmap_file unit test");
+	REQUIRE(file.get_position().str() == line1);
+
+	// Test skip_until(chr)
+	file.skip_until('\n');
+
+	// Test skip(chr)
+	// Skip all new-lines, expect two of them
+	REQUIRE(static_cast<size_t>(2) == file.skip('\n'));
+
+	// Check that we are at line 3
+	const std::string line3("adf lkjdsf asdfkdsafjh sakdfhkjashfd");
+	REQUIRE(file.get_position().str() == line3);
+	REQUIRE(static_cast<size_t>(3) ==
+			file.get_position().line());
+	REQUIRE(static_cast<size_t>(1) ==
+			file.get_position().column());
+
+	// Test skip(str)
+	// Check that skipping all characters on current line takes us
+	// to the new-line character
+	REQUIRE(line3.length() == file.skip(line3.c_str()));
+	REQUIRE(static_cast<size_t>(3) ==
+			file.get_position().line());
+	REQUIRE(line3.length()+1 == file.get_position().column());
 }
+
